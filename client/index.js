9c0dbad68cbf4b2fc15a7b50cc322aea3f1ab49e
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-11-13 17:12:00 +0000

    Update index.js

--- client/index.js
+++ client/index.js
@@ -13,14 +13,10 @@ console.log(
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <div class="title">Pixtopia</div>
-          <div class="sub title">The best game you will probably never play</div>
-          <div class="mini title">And all my other projects</div>
-          <p>Pixtopia was a game I decided to make after a scammer who wanted a big discord server got people to join to create a game. I thought that I could rival it: yeah, no</p>
-        </Switch>
-      </Router>
+      <div class="title">Pixtopia</div>
+      <div class="sub title">The best game you will probably never play</div>
+      <div class="mini title">And all my other projects</div>
+      <p>Pixtopia was a game I decided to make after a scammer who wanted a big discord server got people to join to create a game. I thought that I could rival it: yeah, no</p>
     );
   }
 }
